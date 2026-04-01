9842d5509f72924b785a51b36c0d8cb50acfab21
Malak-web166 <m.mousa0499@student.aast.edu>
2026-04-01 23:16:51 +0200

    Delete Models directory

--- Models/Courses.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication1.Models;
-
-public class Course
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public int InstructorId { get; set; } // Foreign Key
-    public Instructor? Instructor { get; set; }
-}
